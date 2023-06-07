9a0fecfc352b9178e3cd1f294bf53e8609ed7a2c
TGerzenstein <tatigerzens@gmail.com>
2023-06-07 17:21:43 -0300

    modified exercise 1 in POO

--- POO.py
+++ POO.py
@@ -1,23 +1,50 @@
 
-
 class Vehiculo:
-    def __init__(self,marca,ruedas):
-        self.marca = marca
+    def __init__(self,color,ruedas):
+        self.color = color
         self.ruedas = ruedas
 
-    def __str__(self):
-        return "El vehiculo de {} tiene {} ruedas".format(self.marca,self.ruedas)
+    
+    def get_color(self):
+        return self.color
+        
+    def get_ruedas(self):
+        return self.ruedas
 
-class Coche:
-    def __init__(self,marca,ruedas,velocidad,cilindrada):
-        Vehiculo.__init__(self,marca,ruedas)
+
+class Coche(Vehiculo):
+    
+    def __init__(self,color,ruedas,velocidad,cilindrada):
+        super().__init__(color,ruedas)
         self.velocidad = velocidad
         self.cilindrada = cilindrada
-    def __str__(self):
-        return Vehiculo.__str__(self) +" con {} km y {}cc".format(self.velocidad,self.cilindrada)
+    
+    def get_velocidad(self):
+        self.velocidad
+
+    def get_cilindrada(self):
+        self.cilindrada
 
 
 #mi_vehiculo = Vehiculo('ford','5')
 #print(mi_vehiculo)
-mi_coche = Coche('ford','5','20','233')
-print(mi_coche)
+#mi_coche = Coche('rojo',5,20,233)
+#print(mi_coche.get_color())
+
+
+class Camioneta(Coche):
+    
+    def __init__(self,color,ruedas,velocidad,cilindrada,carga):
+        super().__init__(color,ruedas,velocidad,cilindrada)
+        self.carga = carga
+
+    def get_carga(self):
+        return self.carga
+
+    
+mi_camioneta = Camioneta('Rojo',4,20,2.5,300)
+print(mi_camioneta.get_carga()) 
+        
+
+
+
